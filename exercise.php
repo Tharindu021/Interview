e55ef221769f328f57c07d208b2d0b1de23f636a
tharindu021 <tharinduniroshan4@gmail.com>
2025-05-28 16:52:34 +0530

    coding test

---
+++ exercise.php
@@ -0,0 +1,45 @@
+<?php
+    function getAllSlices($people, $slices_per_person)
+    {
+        return $people*$slices_per_person;
+    }
+
+    function getTotalCost($slices, $packages)
+    {
+        $package_prices = [];
+
+        foreach($packages as $name => $package)
+        {
+            $cost = $package[0]*ceil($slices/$package[1]);
+            $package_prices[$name] = $cost;
+        }
+        return $package_prices;
+    }
+
+    function getBestPackage($package_cost_array)
+    {
+        return array_search(min($package_cost_array), $package_cost_array);
+    }
+
+    $packages = array(
+        "Small" => [15, 10],
+        "Medium" => [25, 20],
+        "Large" => [35, 30],
+        "Extra Large" => [50, 50],
+    );
+
+    // get the total slices for all peoples
+    $slices = getAllSlices(20,3);
+    print("Total Slices:" . $slices . "\n\n");
+
+    // get the packages cost for slices
+    $package_cost_array = getTotalCost($slices, $packages);
+    //print the package details
+    foreach ($package_cost_array as $name => $cost) {
+        print($name ." Package: $". $cost . "\n");
+    }
+
+    // get the minimum price package name
+    $best_package_name = getBestPackage($package_cost_array);
+    print("\n" .$best_package_name. " package is best value for the money.");
+?>
